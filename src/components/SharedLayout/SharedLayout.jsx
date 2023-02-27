66f31311be79cfd48151176800153f8714ead4c4
Anatolii-melashchenko <toramulo2012@gmail.com>
2023-02-27 18:19:49 +0200

    first commit

---
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -0,0 +1,21 @@
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+
+import { Container, Header, Link } from './SharedLayout.styled';
+
+export const SharedLayout = () => {
+  return (
+    <Container>
+      <Header>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/products">Movies</Link>
+        </nav>
+      </Header>
+      <Suspense fallback={<div>Loading page...</div>}>
+        <Outlet />
+      </Suspense>
+    </Container>
+  );
+};
